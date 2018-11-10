91e6d840b1c0ac7e0ba88fe7fa4b159cdb892451
miamor <miamor@users.noreply.github.com>
2018-11-10 19:07:13 +0700

    Doimatkhau

---
+++ doimatkhau.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Qlks
+{
+    partial class doimatkhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_mkc = new System.Windows.Forms.TextBox();
+            this.lb_mkc = new System.Windows.Forms.Label();
+            this.lb_tk = new System.Windows.Forms.Label();
+            this.lb_mkm = new System.Windows.Forms.Label();
+            this.txt_mkm = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bt_luu = new System.Windows.Forms.Button();
+            this.bt_dong = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(31, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account";
+            // 
+            // txt_mkc
+            // 
+            this.txt_mkc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mkc.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txt_mkc.Location = new System.Drawing.Point(165, 90);
+            this.txt_mkc.Name = "txt_mkc";
+            this.txt_mkc.Size = new System.Drawing.Size(100, 23);
+            this.txt_mkc.TabIndex = 0;
+            // 
+            // lb_mkc
+            // 
+            this.lb_mkc.AutoSize = true;
+            this.lb_mkc.BackColor = System.Drawing.Color.LightSalmon;
+            this.lb_mkc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_mkc.ForeColor = System.Drawing.Color.Black;
+            this.lb_mkc.Location = new System.Drawing.Point(31, 93);
+            this.lb_mkc.Name = "lb_mkc";
+            this.lb_mkc.Size = new System.Drawing.Size(91, 17);
+            this.lb_mkc.TabIndex = 0;
+            this.lb_mkc.Text = "OldPassword";
+            // 
+            // lb_tk
+            // 
+            this.lb_tk.AutoSize = true;
+            this.lb_tk.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_tk.ForeColor = System.Drawing.Color.Black;
+            this.lb_tk.Location = new System.Drawing.Point(162, 51);
+            this.lb_tk.Name = "lb_tk";
+            this.lb_tk.Size = new System.Drawing.Size(37, 19);
+            this.lb_tk.TabIndex = 0;
+            this.lb_tk.Text = "label";
+            // 
+            // lb_mkm
+            // 
+            this.lb_mkm.AutoSize = true;
+            this.lb_mkm.BackColor = System.Drawing.Color.LightSalmon;
+            this.lb_mkm.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_mkm.ForeColor = System.Drawing.Color.Black;
+            this.lb_mkm.Location = new System.Drawing.Point(31, 136);
+            this.lb_mkm.Name = "lb_mkm";
+            this.lb_mkm.Size = new System.Drawing.Size(99, 19);
+            this.lb_mkm.TabIndex = 0;
+            this.lb_mkm.Text = "NewPassword";
+            // 
+            // txt_mkm
+            // 
+            this.txt_mkm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mkm.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txt_mkm.Location = new System.Drawing.Point(165, 133);
+            this.txt_mkm.Name = "txt_mkm";
+            this.txt_mkm.Size = new System.Drawing.Size(100, 23);
+            this.txt_mkm.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSalmon;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(31, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter the password";
+            // 
+            // txt_nl
+            // 
+            this.txt_nl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nl.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txt_nl.Location = new System.Drawing.Point(165, 177);
+            this.txt_nl.Name = "txt_nl";
+            this.txt_nl.Size = new System.Drawing.Size(100, 23);
+            this.txt_nl.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SeaGreen;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(58, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Change Password";
+            // 
+            // bt_luu
+            // 
+            this.bt_luu.BackColor = System.Drawing.Color.SteelBlue;
+            this.bt_luu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_luu.ForeColor = System.Drawing.Color.AliceBlue;
+            this.bt_luu.Location = new System.Drawing.Point(62, 222);
+            this.bt_luu.Name = "bt_luu";
+            this.bt_luu.Size = new System.Drawing.Size(80, 30);
+            this.bt_luu.TabIndex = 3;
+            this.bt_luu.Text = "Save";
+            this.bt_luu.UseVisualStyleBackColor = false;
+            this.bt_luu.Click += new System.EventHandler(this.bt_luu_Click);
+            // 
+            // bt_dong
+            // 
+            this.bt_dong.BackColor = System.Drawing.Color.SteelBlue;
+            this.bt_dong.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_dong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_dong.ForeColor = System.Drawing.Color.AliceBlue;
+            this.bt_dong.Location = new System.Drawing.Point(185, 222);
+            this.bt_dong.Name = "bt_dong";
+            this.bt_dong.Size = new System.Drawing.Size(80, 30);
+            this.bt_dong.TabIndex = 4;
+            this.bt_dong.Text = "Exit";
+            this.bt_dong.UseVisualStyleBackColor = false;
+            this.bt_dong.Click += new System.EventHandler(this.bt_dong_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Qlks.Properties.Resources.ic;
+            this.pictureBox1.Location = new System.Drawing.Point(238, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // doimatkhau
+            // 
+            this.AcceptButton = this.bt_luu;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Qlks.Properties.Resources.Beautiful_floral_background_Stock_Photo;
+            this.CancelButton = this.bt_dong;
+            this.ClientSize = new System.Drawing.Size(299, 271);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bt_dong);
+            this.Controls.Add(this.bt_luu);
+            this.Controls.Add(this.txt_nl);
+            this.Controls.Add(this.txt_mkm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_mkc);
+            this.Controls.Add(this.lb_mkm);
+            this.Controls.Add(this.lb_tk);
+            this.Controls.Add(this.lb_mkc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(315, 310);
+            this.MinimumSize = new System.Drawing.Size(315, 310);
+            this.Name = "doimatkhau";
+            this.Text = "doimatkhau";
+            this.Load += new System.EventHandler(this.doimatkhau_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_mkc;
+        private System.Windows.Forms.Label lb_mkc;
+        private System.Windows.Forms.Label lb_tk;
+        private System.Windows.Forms.Label lb_mkm;
+        private System.Windows.Forms.TextBox txt_mkm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bt_luu;
+        private System.Windows.Forms.Button bt_dong;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
